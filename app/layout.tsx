09c7d5ef967222e51196b529a00048446c41ff46
Giovanni Gravili <153454774+ghovax@users.noreply.github.com>
2025-11-27 19:31:38 +0100

    Check

--- app/layout.tsx
+++ app/layout.tsx
@@ -2,7 +2,7 @@ import type { Metadata } from "next";
 import {
   Merriweather,
   Merriweather_Sans,
-  Noto_Sans_Mono
+  Noto_Sans_Mono,
 } from "next/font/google";
 import "@/lib/document.css";
 import "./globals.css";
