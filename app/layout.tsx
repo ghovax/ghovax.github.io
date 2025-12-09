0fb584c1ad109d6300f814290e03599bd465f9fe
Giovanni Gravili <153454774+ghovax@users.noreply.github.com>
2025-12-09 22:01:58 +0100

    Check

--- app/layout.tsx
+++ app/layout.tsx
@@ -2,14 +2,55 @@ import type { Metadata } from "next";
 import {
   Merriweather,
   Merriweather_Sans,
+  Noto_Sans,
   Noto_Sans_Mono,
+  Noto_Serif,
+  STIX_Two_Text,
 } from "next/font/google";
+
 import "@/lib/document.css";
 import "./globals.css";
 
-const merriweatherSans = Merriweather_Sans();
-const merriweather = Merriweather();
+const notoSerif = Noto_Serif({
+  weight: "variable",
+  style: ["italic", "normal"],
+  subsets: [
+    "cyrillic",
+    "cyrillic-ext",
+    "greek",
+    "greek-ext",
+    "latin",
+    "latin-ext",
+    "math",
+    "vietnamese",
+  ],
+});
+const notoSans = Noto_Sans({
+  weight: "variable",
+  style: ["italic", "normal"],
+  subsets: [
+    "cyrillic",
+    "cyrillic-ext",
+    "greek",
+    "greek-ext",
+    "latin",
+    "latin-ext",
+    "vietnamese",
+  ],
+});
 const notoSansMono = Noto_Sans_Mono();
+const stixTwoText = STIX_Two_Text({
+  weight: "variable",
+  style: ["italic", "normal"],
+  subsets: [
+    "cyrillic",
+    "cyrillic-ext",
+    "greek",
+    "latin",
+    "latin-ext",
+    "vietnamese",
+  ],
+});
 
 export const metadata: Metadata = {
   title: "Giovanni Gravili's Portfolio",
